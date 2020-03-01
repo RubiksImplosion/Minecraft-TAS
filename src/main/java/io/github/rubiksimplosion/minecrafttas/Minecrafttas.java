8df18be59ad79abcb83b1f89dfb53790ec932997
RubiksImplosion <rubiksexplosion@gmail.com>
2020-03-01 10:34:24 -0600

    initial commit

---
+++ src/main/java/io/github/rubiksimplosion/minecrafttas/Minecrafttas.java
@@ -0,0 +1,10 @@
+package io.github.rubiksimplosion.minecrafttas;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minecrafttas implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
