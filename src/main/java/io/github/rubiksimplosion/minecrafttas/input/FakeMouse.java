586e074631158faa330fe336739170219a428bfa
RubiksImplosion <rubiksexplosion@gmail.com>
2020-03-11 11:13:32 -0500

    Updated hook into Mouse class methods

--- src/main/java/io/github/rubiksimplosion/minecrafttas/input/FakeMouse.java
+++ src/main/java/io/github/rubiksimplosion/minecrafttas/input/FakeMouse.java
@@ -1,22 +1,13 @@
 package io.github.rubiksimplosion.minecrafttas.input;
 
+import io.github.rubiksimplosion.minecrafttas.mixin.MouseAccessor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.Mouse;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 @Environment(EnvType.CLIENT)
 public class FakeMouse {
     public static void fakeMouseButton(int button, int action, int mods) {
-        try {
-            Method onMouseButton = Mouse.class.getDeclaredMethod("onMouseButton", long.class, int.class, int.class, int.class);
-            onMouseButton.setAccessible(true);
-            onMouseButton.invoke(MinecraftClient.getInstance().mouse, MinecraftClient.getInstance().getWindow().getHandle(), button, action, mods);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
+        ((MouseAccessor)MinecraftClient.getInstance().mouse).callOnMouseButton(MinecraftClient.getInstance().getWindow().getHandle(), button, action, mods);
     }
 }
